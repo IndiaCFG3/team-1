d3689ba5c2d4416a0369bb5d5b98d1f8220fcee7
Utkarsh Mishra <utkarsh1148@gmail.com>
2020-08-16 15:50:05 +0530

    'video'

--- cfgbackend/webapp/models.py
+++ cfgbackend/webapp/models.py
@@ -12,3 +12,13 @@ class users(models.Model):
 
     def __str__(self):
         return self.username
+
+
+class videos(models.Model):
+    url=models.CharField(max_length=60)
+    courseID=models.CharField(max_length=20)
+    courseName=models.CharField(max_length=20)
+    organisation=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.url
